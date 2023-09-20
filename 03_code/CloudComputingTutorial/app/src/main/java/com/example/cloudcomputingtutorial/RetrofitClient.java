c0d5af064a6e9b89a2cd7616f60c8614b1cf8b42
jiku100 <tlstjrrud007@naver.com>
2023-09-21 01:27:00 +0900

    Change default URL

--- 03_code/CloudComputingTutorial/app/src/main/java/com/example/cloudcomputingtutorial/RetrofitClient.java
+++ 03_code/CloudComputingTutorial/app/src/main/java/com/example/cloudcomputingtutorial/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private final static String BASE_URL = "http://ec2-43-201-18-96.ap-northeast-2.compute.amazonaws.com:3000"; // Replace EC2 endpoint with yours
+    private final static String BASE_URL = "http://EC2 Endpoint:3000"; // Replace EC2 endpoint with yours
     private static Retrofit retrofit = null;
 
     private RetrofitClient() {

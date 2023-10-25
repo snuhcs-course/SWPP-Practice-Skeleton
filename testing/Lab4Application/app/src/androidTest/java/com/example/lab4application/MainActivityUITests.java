3ba19df7989dd57c5a1903f8ca5319edd8e80547
Hyunsoo Kim <hyunsoo.kim@hcs.snu.ac.kr>
2023-10-25 20:34:29 +0900

    Add first UI testing code

---
+++ testing/Lab4Application/app/src/androidTest/java/com/example/lab4application/MainActivityUITests.java
@@ -0,0 +1,28 @@
+package com.example.lab4application;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityUITests {
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    public void resetButton_click_numRolledZero(){
+        onView(withId(R.id.resetButton))
+                .perform(click());
+        onView(withId(R.id.numRolledTextView))
+                .check(matches(withText("0")));
+    }
+
+}
